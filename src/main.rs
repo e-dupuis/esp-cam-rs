3585a89a34b2510fbb6c1225f9b9590b86df7ffd
e-dupuis <red.life34@gmail.com>
2023-10-28 13:02:29 +0000

    Add wifi

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
+use embedded_svc::wifi::{ClientConfiguration, Configuration};
+use esp_idf_hal::peripherals::Peripherals;
+use esp_idf_svc::{eventloop::EspSystemEventLoop, nvs::EspDefaultNvsPartition, wifi::EspWifi};
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
 use log::*;
+use std::{thread::sleep, time::Duration};
+
+#[toml_cfg::toml_config]
+pub struct Config {
+    #[default("")]
+    wifi_ssid: &'static str,
+    #[default("")]
+    wifi_psk: &'static str,
+}
 
 fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
@@ -8,5 +20,40 @@ fn main() {
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    info!("Hello, world!");
+    // Setup peripherals
+    let peripherals = Peripherals::take().unwrap();
+    let sys_loop: esp_idf_svc::eventloop::EspEventLoop<esp_idf_svc::eventloop::System> =
+        EspSystemEventLoop::take().unwrap();
+    let nvs = EspDefaultNvsPartition::take().unwrap();
+
+    info!("Hello, Théo!");
+
+    let app_config: Config = CONFIG;
+
+    let mut wifi_driver: EspWifi<'_> =
+        EspWifi::new(peripherals.modem, sys_loop, Some(nvs)).unwrap();
+
+    wifi_driver
+        .set_configuration(&Configuration::Client(ClientConfiguration {
+            ssid: app_config.wifi_ssid.into(),
+            password: app_config.wifi_psk.into(),
+            ..Default::default()
+        }))
+        .unwrap();
+
+    wifi_driver.start().unwrap();
+    wifi_driver.connect().unwrap();
+
+    while !wifi_driver.is_connected().unwrap() {
+        let config = wifi_driver.get_configuration().unwrap();
+        println!("Waiting for station {:?}", config);
+    }
+    println!("Should be connected now");
+    loop {
+        println!(
+            "IP info: {:?}",
+            wifi_driver.sta_netif().get_ip_info().unwrap()
+        );
+        sleep(Duration::new(10, 0));
+    }
 }
